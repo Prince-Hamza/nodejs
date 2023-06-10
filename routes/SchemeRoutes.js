70afca731e0977432e5e6db36e39b065c775ce08
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-10 22:13:58 +0500

    update

--- routes/SchemeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { userScheme } from '../controllers/SchemeController.js';
-
-const router = express.Router()
-
-router.post('/createuser', userScheme)
-
-export default router
