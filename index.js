302aaacc32d4cd5c1df1fcc4ca128e8f26f18e41
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-10 21:18:26 +0500

    update

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const http = require('http');
-const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+// es5
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+// const router = require('./api/router.js')
+require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+// app.use('/api', router)
+app.use('/', express.static(__dirname + '/build'))
+
+const port = process.env.PORT
+app.listen(port || 5000)
+console.log(`Server running on port: ${port || 5000}`)
+
+
+
+// es6
+
+
