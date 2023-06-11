2ef4f9270ed14c415b96b97f05c3471dc944eb2d
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-11 14:00:48 +0500

    star

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(__dirname + '/build'))
+app.use('*', express.static(__dirname + '/build'))
+
 
 app.use('/auth', AuthRoute);
 app.use('/user', UserRoute)
