bce8a5c65c800a284976d0d02e54efddadbf7309
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-13 08:33:05 +0500

    update

--- server.js
+++ server.js
@@ -11,10 +11,19 @@ const ChatRoute = require('./routes/ChatRoute.js')
 const MessageRoute = require('./routes/MessageRoute.js')
 const CommentRoute = require('./routes/CommentRoute.js')
 const SchemeRoute = require('./routes/SchemeRoutes.js')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const { MongoClient } = require('mongodb')
 const path = require('path')
 require('dotenv').config()
 
-console.log(`converted to es5`)
+
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+
+
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -24,8 +33,9 @@ app.use(bodyParser.json())
 app.use(express.static(__dirname + '/build'))
 
 app.get("/", function (req, res) {
-   return res.sendFile(path.join(__dirname, "build", "index.html"))
+    return res.sendFile(path.join(__dirname, "build", "index.html"))
 })
+
 
 app.use('/auth', AuthRoute)
 app.use('/user', UserRoute)
@@ -43,11 +53,86 @@ mongoose
     .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         const port = process.env.PORT
-        app.listen(port || 5000)
+        httpServer.listen(port || 5000)
         console.log(`Server running on port: ${port || 5000}`)
         console.log(`Listening @ Port ${PORT} | Mongoose is successfully connected`)
     })
     .catch((error) => console.log(`${error} Mongodb did not connect`));
+
+
+
+
+
+
+// mongodb events 
+
+function closeChangeStream(timeInMs = 60000, changeStream) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Closing the change stream");
+            changeStream.close();
+            resolve();
+        }, timeInMs)
+    })
+}
+
+async function monitorListingsUsingEventEmitter(client, timeInMs = 60000, pipeline = []) {
+    const collection = client.db("test").collection("messages");
+
+    // See https://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html#watch for the watch() docs
+    const changeStream = collection.watch(pipeline);
+
+    // ChangeStream inherits from the Node Built-in Class EventEmitter (https://nodejs.org/dist/latest-v12.x/docs/api/events.html#events_class_eventemitter).
+    // We can use EventEmitter's on() to add a listener function that will be called whenever a change occurs in the change stream.
+    // See https://nodejs.org/dist/latest-v12.x/docs/api/events.html#events_emitter_on_eventname_listener for the on() docs.
+    changeStream.on('change', (data) => {
+        console.log(`changes detected in mongodb: ${JSON.stringify(data)}`)
+        io.emit('message', data)
+    })
+
+    console.log(`listings : waiting for changes in mongodb`);
+
+    // Wait the given amount of time and then close the change stream
+    await closeChangeStream(timeInMs, changeStream);
+}
+
+
+async function mongooseEvents() {
+
+    let uri = process.env.MONGODB_CONNECTION
+    let client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const pipeline = [
+            {
+                '$match': {
+                    'operationType': 'insert'
+                }
+            }
+        ]
+
+        await monitorListingsUsingEventEmitter(client, 60000 * 30, pipeline)
+
+    } finally {
+        await client.close();
+    }
+}
+
+
+// socket events
+
+
+io.on('connection', (socket) => {
+    console.log(`on connection : server connected to soket.io : ${socket.id} `)
+    socket.on('listen', async (data) => {
+        if (data && Object.keys(data).length) {
+            console.log(`LISTEN_EVEN :: Activate a listener for : ${JSON.stringify(data)}`)
+            await mongooseEvents()
+        }
+    })
+})
+
 
 
 
