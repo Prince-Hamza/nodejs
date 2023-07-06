65556831e9cd5fc6f2b2ed88d63fbe29b97551d3
Prince-Hamza <princehamzi.mine@gmail.com>
2023-07-06 13:56:01 +0500

    no cors

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ const { createServer } = require('http')
 const { Server } = require('socket.io')
 const { MongoClient } = require('mongodb')
 const path = require('path')
-const { fileURLToPath } = require('url')
 const UserModel = require("./models/userModel.js")
-
 require('dotenv').config()
 
 const app = express()
 const httpServer = createServer(app)
-const io = new Server(httpServer, { cors: { origin: '*' } });
+const io = new Server(httpServer)
 
-
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -236,56 +233,58 @@ if (!listening) {
 
         console.log(`on connection : server connected to soket.io : ${socket.id} `)
 
-        // console.log(`listening`)
-        // socket.on('listen', async (data) => {
-        //     if (data && Object.keys(data).length) {
-        //         console.log(`LISTEN_EVEN :: Activate a listener for : ${JSON.stringify(data)}`)
-        //         await mongooseEvents()
-        //     }
-        // })
+        console.log(`listening`)
+        socket.on('listen', async (data) => {
+            if (data && Object.keys(data).length) {
+                console.log(`LISTEN_EVEN :: Activate a listener for : ${JSON.stringify(data)}`)
+                await mongooseEvents()
+            }
+        })
 
-        // socket.on('joined', async ({ userId, userName, profilePicture }) => {
-        //     // add to online users
-        //     // save/send online users along socket.id
-        //     console.log(`user : ${userId} has joined`)
-        //     onlineUsers.push({ userId: userId, socketId: socket.id, userName: userName, profilePicture: profilePicture })
-        //     console.log(`emit online users : ${onlineUsers}`)
+        socket.on('joined', async ({ userId, userName, profilePicture }) => {
+            // add to online users
+            // save/send online users along socket.id
+            console.log(`user : ${userId} has joined`)
+            onlineUsers.push({ userId: userId, socketId: socket.id, userName: userName, profilePicture: profilePicture })
+            console.log(`emit online users : ${onlineUsers}`)
 
-        //     await UserModel.findOneAndUpdate({ id: userId }, { online: true })
-        //     console.log(`updated user as online succesfully`)
+            await UserModel.findOneAndUpdate({ id: userId }, { online: true })
+            console.log(`updated user as online succesfully`)
 
-        // })
-
-
-        // socket.on("disconnect", async (data) => {
-
-        //     console.log(`disconnect data : ${data}`)
-        //     console.log(`socket : ${socket.id} is disconnected`)
+        })
 
 
-        //     var userId = false
-        //     onlineUsers.forEach((item) => {
-        //         console.log(`si  (${item.socketId})  === s.i(${socket.id})  or in ${data} ???`)
-        //         if (item.socketId === socket.id) {
-        //             console.log(`FOUND DISCONNECTED USER : Id : ${item.userId} `)
-        //             userId = item.userId
-        //         }
-        //         return item.socketId === socket.id
-        //     })
+        socket.on("disconnect", async (data) => {
 
-        //     if (userId) {
-        //         await UserModel.findOneAndUpdate({ id: userId }, { online: false })
+            console.log(`disconnect data : ${data}`)
+            console.log(`socket : ${socket.id} is disconnected`)
 
-        //         console.log(`updated user : ${userId} as offline succesfully`)
 
-        //         onlineUsers = onlineUsers.filter((item) => { return item.socketId !== socket.id })
-        //     }
+            var userId = false
+            onlineUsers.forEach((item) => {
+                console.log(`si  (${item.socketId})  === s.i(${socket.id})  or in ${data} ???`)
+                if (item.socketId === socket.id) {
+                    console.log(`FOUND DISCONNECTED USER : Id : ${item.userId} `)
+                    userId = item.userId
+                }
+                return item.socketId === socket.id
+            })
 
-        // })
+            if (userId) {
+                await UserModel.findOneAndUpdate({ id: userId }, { online: false })
+
+                console.log(`updated user : ${userId} as offline succesfully`)
+
+                onlineUsers = onlineUsers.filter((item) => { return item.socketId !== socket.id })
+            }
+
+        })
 
     })
 
 }
+
+
 
 
 
